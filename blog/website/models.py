2f0865f56f6ed031668dc0afdb9d0370b444b926
Andre Guilherme de Oliveira Alcantara <andre.alcantara@reitoria.ifpe.edu.br>
2022-01-06 11:14:02 -0300

    Adicionando categorias/Enumeration no sistema do banco de dados. O Djando 3 Possui uma nova feature para enumeration.

--- blog/website/models.py
+++ blog/website/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
 
-# Create your models here.
+class Categories(models.TextChoices):
+    TECH = 'TC', 'Tecnologia'
+    CR = 'CR', 'Curiosidades'
+    GR = 'GR', 'Geral'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(verbose_name='Sub titulo', max_length=200)
     content = models.TextField()
+    categorie = models.CharField(
+        verbose_name='Categories',
+        max_length=2,
+        choices=Categories.choices,
+        default=Categories.GR,
+    )
+
+    def __str__(self):
+        return self.title
